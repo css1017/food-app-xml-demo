8b28e35ab7a87da4352afe94458b44b819f4a542
css101 <css101782@gmail.com>
2024-05-24 21:54:22 +0400

    Added layouts for all screens

--- app/src/main/java/com/css101/foodappxml/MainActivity.kt
+++ app/src/main/java/com/css101/foodappxml/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000) //todo get rid
+        Thread.sleep(1000) //todo get rid
         setTheme(R.style.Theme_FoodiesXmlDemo)
         setContentView(R.layout.activity_main)
     }
